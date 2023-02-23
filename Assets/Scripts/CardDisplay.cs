d7a88b2c4d2bc60c21a8e0ca5b96d2a0b4cab6c3
Aylon1998 <aylon3dg@gmail.com>
2023-02-23 19:50:12 +0200

    First commit.
    Scriptable object Ex.

---
+++ Assets/Scripts/CardDisplay.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using CardGameCode.DataLayer;
+using UnityEngine.UI;
+using TMPro;
+
+namespace CardGameCode.VisualLayer { 
+
+    public class CardDisplay : MonoBehaviour
+    {
+        #region Variables 
+
+        [SerializeField] private TextMeshProUGUI cardDescriptionText, cardStrengthText ,cardNameText;
+        [SerializeField] private Image frontSideImage;
+        [SerializeField] private Scrollbar manaBar;
+
+        public CardData cardData;
+
+        private string ChooseCardName()
+        {
+            switch (cardData.cardType)
+            {
+                case CardData.CardType.PowerUpCard:
+                    return "Power Up Card";
+
+                case CardData.CardType.EventCard:
+                    return "Event Card";
+
+                case CardData.CardType.FailureCard:
+                    return "Failure Card";
+
+                case CardData.CardType.SurvivalCard:
+                    return "Survival Card";
+
+                default:
+                    return "Unknown Card Type";
+            }
+        }
+
+        #endregion
+
+        #region Methods 
+
+        private void Start()
+        {
+            manaBar.size = cardData.CardMana;
+        }
+
+        private void OnValidate()
+        {
+            if (cardData != null)
+            {
+                cardDescriptionText.text = cardData.CardDescription;
+                cardStrengthText.text = cardData.CardStrength.ToString();
+                cardNameText.text = ChooseCardName();
+                frontSideImage.sprite = cardData.frontSideArt;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Assets/Scripts/ScriptableObjectScripts/CardData.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+namespace CardGameCode.DataLayer
+{
+    [CreateAssetMenu(fileName = "GenericCardData", menuName = "Card/GenericCardData")]
+
+        public class CardData : ScriptableObject
+        {
+             #region Variables
+
+            [SerializeField] [Range(1,cardStrengthMaxValue)] private int cardStrength;
+            [SerializeField] [Range(0, cardManaMaxValue)] private float cardMana;
+            [TextArea] [SerializeField] private string cardDescription;
+
+            private const int cardStrengthMaxValue = 10;
+            private const float cardManaMaxValue = 1f;
+
+            public Sprite frontSideArt;
+            public CardType cardType;
+
+            public int CardStrength
+            { 
+                get { return cardStrength; }
+
+                set 
+                {
+                    if(value >= cardStrengthMaxValue)
+                    {
+                        value = cardStrengthMaxValue;
+                    }
+
+                    cardStrength = value;
+                } 
+            }
+
+            public float CardMana
+            {
+                get { return cardMana; }
+
+                set
+                {
+                    if (value >= cardManaMaxValue)
+                    {
+                        value = cardManaMaxValue;
+                    }
+
+                    cardMana = value;
+                }
+            }
+
+            public string CardDescription => cardDescription;
+
+            #endregion
+
+             #region Enums
+
+            public enum CardType
+                 {
+                        PowerUpCard,
+                        EventCard,
+                        FailureCard,
+                        SurvivalCard
+                 }
+            }
+
+            #endregion
+}
